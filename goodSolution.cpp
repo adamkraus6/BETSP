ac180ad894e9ceebc3c17a7b88f63fb02c743cb8
Kraus, Adam <adamkraus6@gmail.com>
2022-10-18 14:52:44 -0600

    aljbvljsrv

--- goodSolution.cpp
+++ goodSolution.cpp
@@ -1,13 +1,49 @@
 // geeks for geeks baby
 // https://www.geeksforgeeks.org/bitonic-traveling-salesman-problem/
 
-// crazy dynamic programming time !
-
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-int main()
+struct Coord {
+	double x,y;
+};
+
+vector<Coord> getCoords(string fileName);
+
+int main(int argc, char** argv)
 {
-    cout << "gonna be an awesome O(n^2) solution" << endl;
+    if(argc != 2)
+	{
+		cout << "BAD" << endl;
+	}
+
+	vector<Coord> coords = getCoords(argv[1]);
+
+	
+}
+
+vector<Coord> getCoords(string fileName)
+{
+    vector<Coord> coords;
+    
+    fstream fin;
+    fin.open(fileName, ios::in);
+
+    int num;
+    fin >> num;
+
+    for(int i = 0; i < num; i++)
+    {
+        Coord coord;
+        fin >> coord.x >> coord.y;
+		coords.push_back(coord);
+    }
+
+    fin.close();
+
+    return coords;
 }
