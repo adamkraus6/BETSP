fa1fd944d5d259573a3a2abe7e0d821ecf60ef8c
mathias314 <mathew.clutter@mines.sdsmt.edu>
2022-10-18 00:11:35 -0600

    adding brute force and n2 solution files

---
+++ goodSolution.cpp
@@ -0,0 +1,13 @@
+// geeks for geeks baby
+// https://www.geeksforgeeks.org/bitonic-traveling-salesman-problem/
+
+// crazy dynamic programming time !
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "gonna be an awesome O(n^2) solution" << endl;
+}
