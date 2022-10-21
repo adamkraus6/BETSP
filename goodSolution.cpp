45c5a5a72e633f5bc782082e5c13023c59425b8d
Kraus, Adam <adamkraus6@gmail.com>
2022-10-21 14:14:28 -0600

    not working

--- goodSolution.cpp
+++ goodSolution.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <iomanip>
+#include <math.h>
 
 using namespace std;
 
@@ -12,31 +14,77 @@ struct Coord {
 	double x,y;
 };
 
-vector<Coord> getCoords(string fileName);
+vector<Coord> coords;
+
+void getCoords(string fileName);
+double dist(int i, int j);
+void outputDP(double dp[200][200], int n);
+double betspTour(double dp[200][200], int i, int j);
 
 int main(int argc, char** argv)
 {
+	double dp[200][200];
+
     if(argc != 2)
 	{
-		cout << "BAD" << endl;
+		cout << "Please add input file argument" << endl;
+		return -1;
 	}
 
-	vector<Coord> coords = getCoords(argv[1]);
+	int n = coords.size();
 
-	
+	for(int j = 1; j < n - 1; j++)
+	{
+		dp[n-1][j] = dist(n-1, n) + dist(j, n);
+	}
+
+	outputDP(dp, n);
+
+	return 0;
 }
 
-vector<Coord> getCoords(string fileName)
+double betspTour(double dp[200][200], int i, int j)
+{
+	if(dp[i][j] > 0)
+	{
+		return dp[i][j];
+	}
+
+	dp[i][j] = min(betspTour(dp, i+1, j) + dist(i, i+1), betspTour(dp, i+1, i));
+
+	return dp[i][j];
+}
+
+double dist(int i, int j)
+{
+	Coord a = coords[i], b = coords[j];
+
+	return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
+
+void outputDP(double dp[200][200], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(8) << setprecision(5) << dp[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void getCoords(string fileName)
 {
     vector<Coord> coords;
     
     fstream fin;
     fin.open(fileName, ios::in);
 
-    int num;
-    fin >> num;
+    int n;
+    fin >> n;
 
-    for(int i = 0; i < num; i++)
+    for(int i = 0; i < n; i++)
     {
         Coord coord;
         fin >> coord.x >> coord.y;
@@ -44,6 +92,4 @@ vector<Coord> getCoords(string fileName)
     }
 
     fin.close();
-
-    return coords;
 }
